91953e9553167d109eb1b625cbb78e2c602b7655
yuvraj11 <63980837+yuvraj11@users.noreply.github.com>
2025-03-08 09:01:06 +0530

    Update PasscodeRepository.java

--- src/main/java/com/example/mywebapp/repository/PasscodeRepository.java
+++ src/main/java/com/example/mywebapp/repository/PasscodeRepository.java
@@ -1,8 +1,10 @@
 package com.example.mywebapp.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.mywebapp.model.Passcode;
 
 public interface PasscodeRepository extends JpaRepository<Passcode, Integer> {
     // JpaRepository gives you all CRUD methods automatically
+    List<Passcode> findAllByOrderByIdDesc();
 }
